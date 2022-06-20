cb696efa14ffef0926fdcc05eb2e11cbb6a2a70f
MaykonFranchini <maykonfranchini2017@gmail.com>
2022-06-20 16:32:20 +0100

    migrations bug fixed

--- prisma/migrations/20220528234549_create_clubs/migration.sql
+++ prisma/migrations/20220528234549_create_clubs/migration.sql
@@ -3,5 +3,6 @@ CREATE TABLE "clubs" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "source_url" TEXT NOT NULL,
-    "logo_url" TEXT NOT NULL
+    "logo_url" TEXT NOT NULL,
+    "location" TEXT NOT NULL DEFAULT 'Brasil'
 );
