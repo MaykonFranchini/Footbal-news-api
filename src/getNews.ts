db01521fe6f92d876fedfecc189a602c89906c77
MaykonFranchini <maykonfranchini2017@gmail.com>
2023-05-14 17:11:37 +0100

    Feature: add latests news from all clubs to DB with cron job

--- src/getNews.ts
+++ src/getNews.ts
@@ -11,23 +11,14 @@ async function saveNewsToDb() {
 
   console.log(`Searching news news`)
   for (const club of clubs) {
-    console.log(`Searching news of ${club.name} - ${club.location}`)
-    if (club.location === 'serie-a') {
-      console.log(`Getting news of ${club.name}`)
-      const latestsNewsList = await latestsNews(club.source_url)
-      console.log(latestsNewsList)
-      for (const news of latestsNewsList) {
-        console.log(`Saving news`)
-
-        await prismaNewsRepository.create({title: news.title, link: news.link, image_url: news.image_url , club_id: club.id, source_name: 'globoesporte', source_img: 'https://s2.glbimg.com/qAiq9Ny0KA4EmGK-aiRLQ8-y3-s=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/f/1/ctmjQ9SOypbLkmmJyMuw/ge.png'})
-      }
-
+    const latestsNewsList = await latestsNews(club.source_url)
+    for (const news of latestsNewsList) {
+      await prismaNewsRepository.create({title: news.title, link: news.link, image_url: news.image_url , club_id: club.id, source_name: 'globoesporte', source_img: 'https://s2.glbimg.com/qAiq9Ny0KA4EmGK-aiRLQ8-y3-s=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/f/1/ctmjQ9SOypbLkmmJyMuw/ge.png'})
     }
   }
 }
 
-const task = cron.schedule(' * 16 * * *', ()=> {
-  console.log('Job Started')
+const task = cron.schedule(' 0 17 * * *', ()=> {
   saveNewsToDb()
 })
 
