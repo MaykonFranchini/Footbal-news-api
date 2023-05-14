d0fcd81a39ea04dfe71e17cc377e5da65232ff31
MaykonFranchini <maykonfranchini2017@gmail.com>
2023-05-14 18:31:54 +0100

    Fix db bugs

--- migrations/20220528234549_create_clubs/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "clubs" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "source_url" TEXT NOT NULL,
-    "logo_url" TEXT NOT NULL,
-    "location" TEXT NOT NULL DEFAULT 'Brasil'
-);
