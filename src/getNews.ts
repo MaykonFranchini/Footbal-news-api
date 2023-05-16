1e14935711e196d5b271dccffa19a4feac7ca639
MaykonFranchini <maykonfranchini2017@gmail.com>
2023-05-16 09:21:08 +0100

    Fix: news image bug fixed

--- src/getNews.ts
+++ src/getNews.ts
@@ -9,16 +9,15 @@ async function saveNewsToDb() {
   const clubs = await prismaClubsRepository.findAll()
   await prismaNewsRepository.deleteAll()
 
-  console.log(`Searching news news`)
   for (const club of clubs) {
     const latestsNewsList = await latestsNews(club.source_url)
     for (const news of latestsNewsList) {
-      await prismaNewsRepository.create({title: news.title, link: news.link, image_url: news.image_url , club_id: club.id, source_name: 'globoesporte', source_img: 'https://s2.glbimg.com/qAiq9Ny0KA4EmGK-aiRLQ8-y3-s=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/f/1/ctmjQ9SOypbLkmmJyMuw/ge.png'})
+      await prismaNewsRepository.create({title: news.title, link: news.link, image_url: news.img_url , club_id: club.id, source_name: 'globoesporte', source_img: 'https://s2.glbimg.com/qAiq9Ny0KA4EmGK-aiRLQ8-y3-s=/0x0:2048x2048/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/f/1/ctmjQ9SOypbLkmmJyMuw/ge.png'})
     }
   }
 }
 
-const task = cron.schedule(' 30 19 * * *', ()=> {
+const task = cron.schedule(' 01 12 * * *', ()=> {
   saveNewsToDb()
 })
 
